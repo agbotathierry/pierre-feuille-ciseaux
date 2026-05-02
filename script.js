4e343dd4a56c00cfc022d82051b95f9111394ae8
Thierry AGBOTA <269857429+agbotathierry@users.noreply.github.com>
2026-05-02 01:42:31 +0000

    jeux de pierre-feuille-ciseaux

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
-console.log("hello world")
+function getComputerChoice(){
+    let nombre  = Math.random();
+    if(nombre<0.33){
+        return "ciseaux";
+
+    }
+    else if(nombre<=0.66){
+        return "pierre";
+    }
+    else {
+        return "feuille"
+    }
+
+}
+function getHumanChoice(){
+    return prompt("veuillez faire un choix entre: pierre-feuille-ciseaux");
+}
+let humanCompteur = 0;
+let computerCompteur=0;
+
+function playgroud(){
+    console.log("debut de manche")
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+    if (computerSelection === humanSelection){
+        console.log("egaliter")
+    }
+    else if ((computerSelection ==="pierre"&& humanSelection==="ciseaux")||
+       (computerSelection === "feuille"&& humanSelection==="pierre")||
+       (computerSelection ==="ciseaux"&& humanSelection==="feuille"))
+       {
+        computerCompteur++;
+        console.log("l'ordi gagne cette manche!")
+       }
+    else{
+        humanCompteur++;
+        console.log("tu a gagner cette manche!")
+    }
+    
+    
+}
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+         console.log("Manche numéro : " + (i+1))
+        playgroud()
+    }
+    console.log("fin de la partie!")
+    // après les 5 manches, on annonce le vainqueur
+    if (humanCompteur > computerCompteur) {
+        console.log("Tu as gagné la partie !")
+    } else if (computerCompteur > humanCompteur) {
+        console.log("L'ordi a gagné la partie !")
+    } else {
+        console.log("Egalité !")
+    }
+}
+
+playGame()

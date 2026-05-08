df6399ced61b8eab7ace60a8dbc8c7a381e9d24f
Thierry AGBOTA <269857429+agbotathierry@users.noreply.github.com>
2026-05-08 23:24:02 +0000

    user interfac

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// mis en place du choix de l'ordinateur
 function getComputerChoice(){
     let nombre  = Math.random();
     if(nombre<0.33){
@@ -12,47 +13,85 @@ function getComputerChoice(){
     }
 
 }
-function getHumanChoice(){
-    return prompt("veuillez faire un choix entre: pierre-feuille-ciseaux");
-}
+//selection des bouton 
+const bouttonPierre = document.querySelector("#pierre");
+const bouttonFeuille=  document.querySelector("#feuille");
+const bouttonCiseaux= document.querySelector("#ciseaux");
+bouttonCiseaux.addEventListener("click",function(){
+    playgroud("ciseaux");
+})
+bouttonFeuille.addEventListener("click", function(){  // appele des choix
+    playgroud("feuille");
+})
+bouttonPierre.addEventListener("click",function(){
+    playgroud("pierre");
+})
+const resultat = document.querySelector(".resultat")
+const score = document.querySelector(".score");
+
+
+// les deux compteur
 let humanCompteur = 0;
 let computerCompteur=0;
+// la fonction coeur du jeu
 
-function playgroud(){
-    console.log("debut de manche")
-    humanSelection = getHumanChoice();
+function playgroud(humanSelection){
+
     computerSelection = getComputerChoice();
     if (computerSelection === humanSelection){
-        console.log("egaliter")
+        resultat.textContent="egualiter"
+        score.textContent = humanCompteur.toString()+"point(s)"+" "+computerCompteur.toString()+"point(s)";
     }
     else if ((computerSelection ==="pierre"&& humanSelection==="ciseaux")||
        (computerSelection === "feuille"&& humanSelection==="pierre")||
        (computerSelection ==="ciseaux"&& humanSelection==="feuille"))
        {
+        // computer
         computerCompteur++;
-        console.log("l'ordi gagne cette manche!")
+        resultat.textContent="l'ordinateur a gagner cette manche!"
+        score.textContent = "ORDINATEUR:"+computerCompteur.toString()+"point(s)"+" "+"HUMAIN:"+humanCompteur.toString()+"point(s)";
        }
     else{
         humanCompteur++;
-        console.log("tu a gagner cette manche!")
+        resultat.textContent="tu a gagner cette manche!"
+        score.textContent ="HUMAIN:"+ humanCompteur.toString()+"point(s)"+" "+"ORDINATEUR:"+computerCompteur.toString()+"point(s)";
+        
     }
+   
+
+    if (humanCompteur===5){
+        resultat.textContent = "tu a gagner la partie";
+        bouttonCiseaux.disabled = true;
+        bouttonPierre.disabled = true;
+        bouttonFeuille.disabled = true;
+        boutonRejouer.style.display= "block"
+      
+            }
     
+    
+    else if (computerCompteur===5){
+        resultat.textContent = "l'ordinateur gagne la parti!";
+        bouttonCiseaux.disabled = true;
+        bouttonPierre.disabled = true;
+        bouttonFeuille.disabled = true;
+        boutonRejouer.style.display= "block"
+    }
     
 }
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-         console.log("Manche numéro : " + (i+1))
-        playgroud()
-    }
-    console.log("fin de la partie!")
-    // après les 5 manches, on annonce le vainqueur
-    if (humanCompteur > computerCompteur) {
-        console.log("Tu as gagné la partie !")
-    } else if (computerCompteur > humanCompteur) {
-        console.log("L'ordi a gagné la partie !")
-    } else {
-        console.log("Egalité !")
-    }
+boutonRejouer = document.querySelector(".rejouer")
+boutonRejouer.addEventListener("click",function(){
+                reset()  
+    })
+// la fonction de relance du jeu
+function reset(){
+    humanCompteur =0;
+    computerCompteur=0;
+    bouttonCiseaux.disabled = false;
+    bouttonPierre.disabled = false;
+    bouttonFeuille.disabled = false;
+    boutonRejouer.style.display= "none";
+    resultat.textContent = "";
+    score.textContent = "";
 }
 
-playGame()
+
